48961509ea7af49da1fdc6d784b172d5a50cfa20
lightclient <lightclient@protonmail.com>
2023-10-05 10:22:13 -0600

    all: add several tests, complete role handling

--- src/Token.sol
+++ src/Token.sol
@@ -1,58 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract Alignment is ERC20, Ownable {
-    mapping(address => bool) public isUnaligned;
+contract AlignmentToken is ERC20, AccessControl {
+    bytes32 public constant MINISTER_OF_ALIGNMENT = keccak256("MINISTER");
+    mapping(address => bool) public excommunicated;
 
     constructor() ERC20("Alignment", "ALIGN") {
-        _mint(msg.sender, 18446744073709552000 * 10 ** decimals());
+        _mint(msg.sender, 120241967 * 10 ** decimals());
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINISTER_OF_ALIGNMENT, msg.sender);
+    }
+
+    // onlyOrdained protects methods which are only to be called by trusted
+    // members of the church, er, the community.
+    modifier onlyOrdained() {
+            _checkRole(MINISTER_OF_ALIGNMENT);
+            _;
+    }
+
+    // ordain confers order of alignment upon the controller of the specified
+    // address.
+    function ordain(address _addr) public onlyOrdained {
+            grantRole(MINISTER_OF_ALIGNMENT, _addr);
     }
 
     // aligned sends an alignment reward to an individual who has expressed an
     // aligned sentiment or performed an aligned behaviour.
-    function aligned(address _addr, uint256 _amt) public onlyOwner {
+    function aligned(address _addr, uint256 _amt) public onlyOrdained {
         _mint(_addr, _amt);
     }
 
     // unaligned is used to penalize small to medium lapses in alignment.
-    function unaligned(address _addr, uint256 _amt) public onlyOwner {
+    function unaligned(address _addr, uint256 _amt) public onlyOrdained {
+        require(!hasRole(MINISTER_OF_ALIGNMENT, _addr), "the ordained may not be penalized");
         _burn(_addr, _amt);
     }
 
     // excommunicate is used in situations where an individuals expresses
     // unaligned and shows no remorse.
-    function excommunicate(address _addr) public onlyOwner {
-        require(_addr != address(0), "Null address is the holy user-space burn location (aligned)");
-        isUnaligned[_addr] = true;
+    function excommunicate(address _addr) public onlyOrdained {
+        require(!hasRole(MINISTER_OF_ALIGNMENT, _addr), "the ordained may not be penalized");
+        require(_addr != address(0), "null address is the holy user-space burn location (aligned)");
+        _burn(_addr, balanceOf(_addr));
+        excommunicated[_addr] = true;
     }
 
     // reconcile allows the excommunicated to regain aligned status.
     function reconcile() external payable {
-        require(msg.value > 10 ether, "Tithe not sufficient");
+        require(msg.value >= 10 ether, "tithe not sufficient");
         payable(address(0)).transfer(msg.value);
-        isUnaligned[msg.sender] = false;
+        excommunicated[msg.sender] = false;
     }
 
     // isUnaligned is a view-only function which allows the community to know
     // the character of the person they are interacting with.
     function isUnaligned(address _addr) public view returns (bool) {
-        return isBlacklisted[_addr];
+        return excommunicated[_addr];
     }
 
     // override
     function transfer(address _to, uint256 _amt) public override returns (bool) {
-        require(!isUnaligned[msg.sender], "Sender is unaligned");
-        require(!isUnaligned[_to], "Recipient is unaligned");
+        require(!excommunicated[msg.sender], "sender is unaligned");
+        require(!excommunicated[_to], "recipient is unaligned");
         return super.transfer(_to, _amt);
     }
 
     // override
     function transferFrom(address _from, address _to, uint256 _amt) public override returns (bool) {
-        require(!isUnaligned[msg.sender], "Sender is unaligned");
-        require(!isUnaligned[to], "Recipient is unaligned");
+        require(!excommunicated[msg.sender], "sender is unaligned");
+        require(!excommunicated[_to], "recipient is unaligned");
         return super.transferFrom(_from, _to, _amt);
     }
 }
--- test/Token.t.sol
+++ test/Token.t.sol
@@ -2,23 +2,81 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Token} from "../src/Token.sol";
+import {AlignmentToken} from "../src/Token.sol";
+
+address constant aa = address(0xaa);
+address constant bb = address(0xbb);
 
 contract TokenTest is Test {
-    Token public token;
+    AlignmentToken public token;
 
     function setUp() public {
-        token = new Token();
-        token.setNumber(0);
+        token = new AlignmentToken();
     }
 
-    function testIncrement() public {
-        token.increment();
-        assertEq(token.number(), 1);
+    function testName() public {
+        assertEq(token.name(), "Alignment");
+        assertEq(token.symbol(), "ALIGN");
     }
 
-    function testSetNumber(uint256 x) public {
-        token.setNumber(x);
-        assertEq(token.number(), x);
+    function testTransfer() public {
+            assertEq(token.balanceOf(aa), 0);
+            token.transfer(aa, 42);
+            assertEq(token.balanceOf(aa), 42);
+    }
+
+    function testIncentives() public {
+            assertEq(token.balanceOf(aa), 0);
+            token.aligned(aa, 100);
+            assertEq(token.balanceOf(aa), 100);
+
+            token.unaligned(aa, 99);
+            assertEq(token.balanceOf(aa), 1);
+
+            vm.prank(bb);
+            vm.expectRevert();
+            token.aligned(aa, 1);
+    }
+
+    function testExcommunication() public {
+            token.transfer(aa, 42);
+            assertEq(token.balanceOf(aa), 42);
+
+            token.excommunicate(aa);
+            assertEq(token.isUnaligned(aa), true);
+
+            vm.expectRevert("recipient is unaligned");
+            token.transfer(aa, 42);
+
+            vm.prank(aa);
+            vm.expectRevert("sender is unaligned");
+            token.transfer(bb, 42);
+
+            vm.deal(aa, 10 ether);
+            vm.prank(aa);
+            token.reconcile{value: 10 ether}();
+            assertEq(token.isUnaligned(aa), false);
+
+            assertEq(token.balanceOf(aa), 0);
+            token.transfer(aa, 42);
+            assertEq(token.balanceOf(aa), 42);
+    }
+
+    function testOrdained() public {
+            assertFalse(token.hasRole(token.MINISTER_OF_ALIGNMENT(), aa));
+            token.ordain(aa);
+            assertTrue(token.hasRole(token.MINISTER_OF_ALIGNMENT(), aa));
+
+            vm.prank(aa);
+            token.aligned(aa, 100);
+
+            vm.expectRevert("the ordained may not be penalized");
+            token.unaligned(aa, 10);
+
+            vm.expectRevert("the ordained may not be penalized");
+            token.excommunicate(aa);
+
+            token.revokeRole(token.MINISTER_OF_ALIGNMENT(), aa);
+            assertFalse(token.hasRole(token.MINISTER_OF_ALIGNMENT(), aa));
     }
 }
